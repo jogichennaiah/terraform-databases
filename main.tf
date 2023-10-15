740cb34a08fc2fc77c2af576210ce588e810215e
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-15 11:29:33 +0530

    Auto push

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
 module "docdb" {
     source               = "./vendor/modules/docdb"
     ENV                  = var.ENV  
-    variable "VPC_CIDR" {}
-    variable "ENV" {}
-    variable "AZ" {}
-    variable "PUBLIC_SUBNET_CIDR" {}
-    variable "PRIVATE_SUBNET_CIDR" {}
-    variable "DEFAULT_VPC_ID" {}
-    variable "DEFAULT_VPC_CIDR" {}
-    variable "DEFAILT_VPC_RT" {}
 }
