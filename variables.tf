2db8d59cb37cd4b404b12e113e905bdfe09fe2b4
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-17 23:50:50 +0530

    Auto push

--- variables.tf
+++ variables.tf
@@ -1,4 +1,10 @@
+variable "VPC_CIDR" {}
 variable "ENV" {}
-
+variable "AZ" {}
+variable "PUBLIC_SUBNET_CIDR" {}
+variable "PRIVATE_SUBNET_CIDR" {}
+variable "DEFAULT_VPC_ID" {}
+variable "DEFAULT_VPC_CIDR" {}
+variable "DEFAILT_VPC_RT" {}
 
 
